b67c7054574204ad8fdebf0c60ab2d9f9621b775
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-01-20 13:26:28 +0100

    script

---
+++ script.js
@@ -0,0 +1,145 @@
+
+// let numero= 65;
+// console.log("prima assegnazione numero",numero);
+
+// numero=5;
+// console.log("seconda assegnazione numero",numero);
+
+// const costante= 12;
+// console.log("log costante",costante)
+// costante=15;
+// console.log( "secondo log costante",costante);
+
+// var numero2=37;
+// console.log("log var", numero2);
+// numero2=145;
+// console.log("log var 2",numero2);
+
+// let pippo= "ciao a tutti";
+// console.log("log pippo",pippo);
+
+// pippo="ciao ad alcuni";
+// console.log("secondo log pippo", pippo);
+
+
+// let operazione=5+6;
+
+// console.log("log operazione", operazione);
+
+
+// if((operazione<10)|| (operazione!==11)){
+// console.log("sono passato dall'if")
+
+// }else
+
+// {  
+// console.log("non sono passato dall'if")
+// console.log("IF")
+
+
+// }
+
+
+// let numero2=5;
+// if (numero2>0){
+// console.log("il numero è positivo");
+// }
+// else{
+
+//     console.log("il numero è negativo");
+// }
+
+
+// console.log("if")
+
+
+
+// let numero3=5;
+// if (numero3>0){
+// console.log("il numero è positivo");
+// }
+// else if  (numero3===0){ 
+
+//     console.log("il numero è 0");
+// }
+// else{
+
+// console.log("il numero è negativo")
+
+// }
+// console.log("Else If")
+
+
+
+// let numero4= 17;
+
+// if (numero4>=0 ){
+
+//     console.log("maggiore o uguale a 0");
+
+// }else if (numero4>10){
+
+//     console.log("maggiore di 10");
+// } else if(numero4>=0){
+
+//     console.log("maggiore o uguale a 0");
+// }else{
+//     console.log("minore di 9");
+// }
+
+
+// let numero5=4;
+
+// if( numero5 % 2===0){
+
+//     console.log("divisibile per le 2 ");
+// }if (numero5 % 3===0){
+//     console.log( "divisibile per 3");
+// }
+
+
+// let i= 0;
+
+// while (i<10) { 
+//     console.log(i);
+//     i=i+1;
+
+    
+// }
+// console.log("While")
+
+
+// let j=0 
+// do {
+//     console.log(j);
+//     j=j+1;
+// } while (j>10);
+
+// console.log("Do While")
+
+
+// for (let k =0 ; k <10; k++) {
+//  console.log(k);
+    
+// }
+// console.log("For")
+
+
+
+
+
+// for (let triangolo  = '#';  triangolo<7 ; triangolo++) {
+//    console.log(triangolo);
+    
+// }
+
+
+let i='#'
+while (!(i==='########')) {
+    console.log(i);
+    i=i+'#'
+    
+}
+
+
+
